b8513bc1c968932846031e0b9e73186fbe601917
SonJava10Rikkei <sonjapan7@gmail.com>
2023-06-01 00:34:12 +0700

    Upload file ok

--- src/main/java/rikkei/academy/config/AppConfig.java
+++ src/main/java/rikkei/academy/config/AppConfig.java
@@ -1,9 +1,13 @@
 package rikkei.academy.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
@@ -11,9 +15,12 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import rikkei.academy.service.CustomerServiceIMPL;
 import rikkei.academy.service.ICustomerService;
 
+import java.io.IOException;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("rikkei.academy.controller")
+@PropertySource("classpath:upload_file.properties")
 public class AppConfig implements WebMvcConfigurer {
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
@@ -43,5 +50,24 @@ public class AppConfig implements WebMvcConfigurer {
     public ICustomerService customerService() {
         return new CustomerServiceIMPL();
     }
+
+    //Cấu hình upload file
+    @Value("${file-upload}")
+    private String fileUpload;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:" + fileUpload);
+
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver getResolver() throws IOException {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSizePerFile(52428800);
+        return resolver;
+    }
+
 
 }
--- src/main/java/rikkei/academy/controller/CustomerController.java
+++ src/main/java/rikkei/academy/controller/CustomerController.java
@@ -1,16 +1,22 @@
 package rikkei.academy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import rikkei.academy.dto.CustomerDTO;
 import rikkei.academy.model.Customer;
 import rikkei.academy.service.ICustomerService;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -18,6 +24,9 @@ import java.util.List;
 public class CustomerController {
     @Autowired
     private ICustomerService customerService;
+    //Cấu hình upload file
+    @Value("${file-upload}")
+    private String fileUpload;
 
     @GetMapping("/")
     public String showListCustomers(Model model) {
@@ -34,14 +43,22 @@ public class CustomerController {
     }
 
     @GetMapping("/create")
-    public String showCreate(Model model) {
-        Customer customer = new Customer();
+    public String showFormCreate(Model model) {
+        CustomerDTO customer = new CustomerDTO();
         model.addAttribute("customer", customer);
         return "customer/create";
     }
 
     @PostMapping("/create")
-    public String actionCreate(Customer customer, RedirectAttributes rs) {
+    public String actionCreate(CustomerDTO customerDTO, RedirectAttributes rs) {
+        MultipartFile multipartFile = customerDTO.getAvatar();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(multipartFile.getBytes(), new File(fileUpload + fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Customer customer = new Customer(customerDTO.getId(), customerDTO.getName(), fileName);
         customerService.save(customer);
         rs.addFlashAttribute("validate", "them moi ok");
         return "redirect:/";
@@ -49,13 +66,27 @@ public class CustomerController {
 
     @GetMapping("/update/{id}")
     public String updateCustomer(@PathVariable Long id, Model model) {
+        CustomerDTO customerDTO = new CustomerDTO();
         Customer customer = customerService.findById(id);
-        model.addAttribute("customer", customer);
+        customerDTO.setId(customer.getId());
+        customerDTO.setName(customer.getName());
+        model.addAttribute("customer", customerDTO);
+        model.addAttribute("avatar", customer.getAvatar());
         return "customer/edit";
     }
 
     @PostMapping("/update")
-    public String actionUpdate(Customer customer, RedirectAttributes rs) {
+    public String actionUpdate(CustomerDTO customerDTO, RedirectAttributes rs) {
+        MultipartFile multipartFile = customerDTO.getAvatar();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(multipartFile.getBytes(), new File(fileUpload + fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Customer customer = customerService.findById(customerDTO.getId());
+        customer.setName(customerDTO.getName());
+        customer.setAvatar(fileName);
         customerService.save(customer);
         rs.addFlashAttribute("validate", "sua ok");
         return "redirect:/";
